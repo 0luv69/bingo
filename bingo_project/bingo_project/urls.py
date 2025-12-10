8feebea51e4a871f4a33ab1aed2022cb15bd3a1e
0luv69 <baniyarujal@gmail.com>
2025-12-10 19:29:10 +0545

    next update phase started

--- bingo_project/bingo_project/urls.py
+++ bingo_project/bingo_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import RedirectView
+from django.conf import settings
+
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
     path('admin/', admin.site.urls),
     path('', include('game.urls')),   
 ]
--- bingo_project/theme/static_src/tailwind.config.js
+++ bingo_project/theme/static_src/tailwind.config.js
@@ -14,7 +14,16 @@ module.exports = {
 
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily:  { 'sans': ['Inter', 'system-ui', 'sans-serif'] },
+                    colors: {
+                        'accent': {
+                            50: '#f0f9ff', 100: '#e0f2fe', 200: '#bae6fd',
+                            300: '#7dd3fc', 400: '#38bdf8', 500: '#0ea5e9',
+                            600: '#0284c7', 700: '#0369a1',
+                        }
+                    }
+                },
     },
     plugins: [
         require('@tailwindcss/forms'),
