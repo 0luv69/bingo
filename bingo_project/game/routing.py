25218caa0898c2e10da8e290d32b64467861d957
0luv69 <baniyarujal@gmail.com>
2025-12-17 07:53:47 +0545

    back to original, moved backed

--- bingo_project/game/routing.py
+++ bingo_project/game/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from . import consumers
+from .import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/room/(?P<room_code>\w+)/$', consumers.GameConsumer.as_asgi()),
